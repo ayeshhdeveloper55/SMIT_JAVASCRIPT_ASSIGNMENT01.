195ac08b4b9edaaf74f399874f1e4bd77c3a4769
AYESHA ASGHAR <ayeshh7766@gmail.com>
2024-12-13 06:05:49 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+
+// VARIABLES FOR STRINGS
+
+
+// QUESTION 01
+// 1. Declare a variable called username.
+// ANSWER 01
+// var userName;
+// QUESTION 02
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// ANSWER 02
+// var myName : "AyeshaAsghar"
+// QUESTION 03
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// ANSWER 03 
+// PART a
+// var tittleMessage;
+// PART b
+// var tittleMessage: "hellowWorld"
+// PART c
+// alert("helloWorld")
+// QUESTION 04
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+// ANSWER 04
+// var studentName = "ayesha"
+// alert("ayesha")
+// var studentAge = 15
+// alert('15')
+// var studentCertificate ="certified intro to programing"
+// alert("allow for certificate")
+
+// QUESTION 05
+// 5. Write a script to display the following alert using one JS
+// variable:
+// ANSWER 05
+// var studenInformation =" Name:ayesha\nclass:12th\nage:20\nschool:abc;"
+// QUESTION 06
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+// ANSWER 06
+// var email: "email@example.com"
+// alert("my email adress is :"+email);
+// QUESTION 07
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+// ANASWER 07
+// var book="a smarter way to learn javascript"
+// alert("i'' m reading' " + book +" to improve my js skills");
+// QUESTION 08
+// 8. Write a script to display this in browser through JS
+// ANSWER 08
+// Document.Write("YA I CAN WRITE CONTENT")
+// QUESTION 09
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+// ANSWER 09
+// var message = "ya i can write "
+// alert(message);
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,51 @@
+//    ALERTS 
+    
+    
+    // QUESTION 01
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+// ANSWER 01
+// alert("welcome to our page");
+
+// QUESTION 02
+// 2. Write a script to display following message on your web
+// page:
+// ANSWER 02
+// Document.write("welcome to our page");
+
+// QUESTION 03
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+// ANSWER 03
+// alert("welcome here\nhappy coding");
+
+// QUESTION 04
+// 4. Write a script to display following messages in sequence:
+// ANSWER 04
+// alert("hello world")
+// alert ("happy coding")
+// alert ("welcome here")
+
+// QUESTION 05
+// 5. Generate the following message through browser’s
+// developer console:
+// ANSWER 05
+// Console.log("I can run js through my web browser's console");
+
+// QUESTION 06
+// 6. Make use of alerts in your new/existing HTML & CSS
+// project.
+// ANSWER 06
+
+
+// QUESTION 07
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+// ANSWER 07
+// answer is html page
+
+
